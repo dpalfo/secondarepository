0da2ffd54b6f01be48cdbb90f5b8df9e612dda81
emistef <emistef@LAPTOP-LL44GDDE>
2018-10-29 12:25:47 +0100

    classe merce

---
+++ secondarepository/src/it/magaz/model/Merce.java
@@ -0,0 +1,62 @@
+package it.magaz.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Merce<T> {
+	
+	private T contenuto;
+	
+	public Merce(T t) {
+		
+		contenuto=t;
+	}
+	
+	static int quantita;
+
+	static List<Mezzo> mezziMerce;
+	
+	static List<Operaio> operaiMerce;
+
+	public T getContenuto() {
+		return contenuto;
+	}
+
+	public void setContenuto(T contenuto) {
+		this.contenuto = contenuto;
+	}
+
+	public static int getQuantita() {
+		return quantita;
+	}
+
+	public static void setQuantita(int quantita) {
+		Merce.quantita = quantita;
+	}
+
+	public static List<Mezzo> getMezziMerce() {
+		return mezziMerce;
+	}
+
+	public static void setMezziMerce(List<Mezzo> mezziMerce) {
+		Merce.mezziMerce = mezziMerce;
+	}
+
+	public static List<Operaio> getOperaiMerce() {
+		return operaiMerce;
+	}
+
+	public static void setOperaiMerce(List<Operaio> operaiMerce) {
+		Merce.operaiMerce = operaiMerce;
+	}
+
+	@Override
+	public String toString() {
+		return "Merce [contenuto=" + contenuto + "]";
+	}
+	
+	
+	
+	
+
+}
---
+++ secondarepository/src/it/magaz/model/MerceInformatica.java
@@ -0,0 +1,32 @@
+package it.magaz.model;
+
+public class MerceInformatica {
+	
+	long id;
+	double peso;
+	double costo;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public double getPeso() {
+		return peso;
+	}
+	public void setPeso(double peso) {
+		this.peso = peso;
+	}
+	public double getCosto() {
+		return costo;
+	}
+	public void setCosto(double costo) {
+		this.costo = costo;
+	}
+	@Override
+	public String toString() {
+		return "MerceCibo [id=" + id + ", peso=" + peso + ", costo=" + costo + "]";
+	}
+	
+
+}
