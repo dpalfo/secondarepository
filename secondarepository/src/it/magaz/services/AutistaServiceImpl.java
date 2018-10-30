35335ee5cef97c1c4c3f010b2f12b19c7736e16c
Utente <Utente@LENOVO-AMD>
2018-10-30 10:10:29 +0100

    vediMezzi

--- secondarepository/src/it/magaz/services/AutistaServiceImpl.java
+++ secondarepository/src/it/magaz/services/AutistaServiceImpl.java
@@ -1,14 +1,21 @@
 package it.magaz.services;
 
+import it.magaz.model.Autista;
 import it.magaz.model.Direttore;
+import it.magaz.model.Mezzo;
 
 public class AutistaServiceImpl implements AutistaService {
 
 	@Override
 	public void vediMezzi(Direttore direttore) {
 		
-		
-		
+		for(Autista i:direttore.getListaAutisti())
+		{
+			for(Mezzo<?> j: i.getListmezaut())
+			{
+				System.out.println(j);
+			}		
+		}
 	}
 
 }
